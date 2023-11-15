b846dbd39d4233b6b9b0a4c74ce22870f0ea586f
Luis Rodriguez <82296579+Driroluis@users.noreply.github.com>
2023-11-15 03:20:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,16 @@
-// Create web server for comment 
-// Import module 
+Synthesizing 9/10 solutions (Duplicates hidden) 
 
+Suggestion 1
+
+// Create web server for comment 
+//======================================
+
+// Import module 
+ 
 const express = require('express'); 
 const router = express.Router(); 
-const comentController = require("controllers/comment");
+const comentController = require("../controllers/comment");
+
 // Handle request 
 router. get( '/' , commentControlter. comment_list) ; 
 router. get( ' /create' , commentController.comment_create_ge );
